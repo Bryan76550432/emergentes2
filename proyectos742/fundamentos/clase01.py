a6a9129d888227d23166929a60bbd5d8e157ee8b
Bryan76550432 <br006167@gmail.com>
2025-08-12 22:25:32 -0400

    Add files via upload

---
+++ proyectos742/fundamentos/clase01.py
@@ -0,0 +1,25 @@
+print("HOLA MUNDO DESDE PYTHON")
+# variable de tipo cadena
+nombre="bruno diaz"
+# variable de tipo entero
+edad= 30
+# variable de tipo flotante
+altura=1.75
+# variable de tipo booleano
+es_estudienate=True
+
+# imprimir las variables 
+print("impresion tradicional")
+print("----------------------")
+print("nombre", nombre)
+print("edad", edad)
+print("altura", altura)
+print("es_estudienate", es_estudienate)
+
+# imprecion tradicional
+print("impresion moderna con f-strings")
+print("----------------------")
+print(f"nombre: {nombre}")
+print(f"edad:{edad}")
+print(f"altura:{altura}")
+print(f"es_estudienate:{es_estudienate}")
